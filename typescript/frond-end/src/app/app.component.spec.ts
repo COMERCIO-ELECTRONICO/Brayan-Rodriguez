63f7f09785f1d3d4c5a1963b3043b776e278d12f
Bryan <rodriguezb787@gmail.com>
2020-05-15 11:21:48 -0500

    Angular inicio

--- typescript/frond-end/src/app/app.component.spec.ts
+++ typescript/frond-end/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front-end'`, () => {
+  it(`should have as title 'frond-end'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('front-end');
+    expect(app.title).toEqual('frond-end');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('front-end app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('frond-end app is running!');
   });
 });
--- typescript/frond-end/src/app/app.module.ts
+++ typescript/frond-end/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiPrimerComponenteComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule // modulo de formularios
   ],
   providers: [],
   bootstrap: [AppComponent]
